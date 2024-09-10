28359e07ca1cd82d493723f677993b4f47074701
pengcuo <pengcbupt@163.com>
2024-09-10 10:48:25 +0800

    Create __init__.py

---
+++ FlagAttention/src/flag_attn/__init__.py
@@ -0,0 +1,15 @@
+try:
+    from ._version import version as __version__
+    from ._version import version_tuple
+except ImportError:
+    __version__ = "0.0.0"
+    version_tuple = (0, 0, 0)
+
+
+from flag_attn.piecewise import attention as piecewise_attention # noqa: F401
+from flag_attn.flash import attention as flash_attention # noqa: F401
+from flag_attn.split_kv import attention as flash_attention_split_kv # noqa: F401
+from flag_attn.paged import attention as paged_attention # noqa: F401
+from flag_attn.mla_paged import mla_attention as paged_mla_attention # noqa: F401
+
+from flag_attn import testing # noqa: F401
